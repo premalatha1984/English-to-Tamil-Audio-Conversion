7b75977be750ca57d4974a3eec8c0687db979826
ajiths <ajiths@agilecyber.com>
2024-05-15 14:54:45 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
+import os
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    with open(f"uploaded_files/{file.filename}", "wb") as buffer:
+    upload_folder = "uploaded_files"
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+    with open(os.path.join(upload_folder, file.filename), "wb") as buffer:
         buffer.write(await file.read())
     return {"filename": file.filename}
